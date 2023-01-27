1af1f3c931c52b441c77c616d9d7c0f1290352ea
AliSeyfullazadaa <116652745+AliSeyfullazadaa@users.noreply.github.com>
2023-01-27 19:34:48 +0400

    Add files via upload

---
+++ Seralize/SerializationDeserialization/SerializationDeserialization/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using SerializationDeserialization;
+
+
+
+async Task<List<User>> GetUsers()
+{
+    List<User> users = new List<User>();
+
+    using (HttpClient httpClient = new HttpClient())
+    {
+        using (var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts"))
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            users = JsonConvert.DeserializeObject<List<User>>(content);
+
+        }
+    }
+    return users.ToList();
+}
+var result = await GetUsers();
+foreach (var item in result)
+{
+    Console.WriteLine($"Item id: {item.id} Item title:{item.title}  Item body:{item.body}");
+    Console.WriteLine("------------------");
+}
+
+
+string filepath = @"C:\Users\User\Desktop\CodeAcademy\Seralize\SerializationDeserialization\SerializationDeserialization\Files\jsonFile.json";
+var jsonString = JsonConvert.SerializeObject(result);
+
+File.WriteAllText(filepath, jsonString);
